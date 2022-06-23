693a935325f989ae4a39be83cc6d0e3d9817718d
Khushboo <kushbooagarwal3584@gmail.com>
2022-06-23 20:52:52 +0530

    updated week 1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,74 +3,78 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'Code for GovTech projects',
-  tagline: 'Updates about activites done by Code for GovTech students.',
-  url: 'http://tech.samagragovernance.in/',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'Samagra',
-  projectName: 'c4gt',
+  title: "Code for GovTech projects",
+  tagline: "Updates about activites done by Code for GovTech students.",
+  url: "http://tech.samagragovernance.in/",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "Samagra",
+  projectName: "c4gt",
   themeConfig: {
     navbar: {
-      title: 'Home',
+      title: "Home",
       logo: {
-        alt: 'Home',
-        src: 'img/logo.png',
+        alt: "Home",
+        src: "img/logo.png",
       },
       items: [
         {
-          label: '2022',
-          position: 'left',
-          to: '/docs/2022/',
+          label: "2022",
+          position: "left",
+          to: "/docs/2022/",
           items: [
             {
-              label: 'UCI Web Channel',
-              to: '/docs/2022/uci/',
+              label: "UCI Web Channel",
+              to: "/docs/2022/uci/",
             },
             {
-              label: 'Competency Passbook',
-              to: '/docs/2022/passbook/',
+              label: "Competency Passbook",
+              to: "/docs/2022/passbook/",
             },
             {
-              label: 'Admin for Sunbird RC',
-              to: '/docs/2022/admin/',
-            }            
-          ]
-        }            
+              label: "Admin for Sunbird RC",
+              to: "/docs/2022/admin/",
+            },
+            {
+              label: "Centralized Access Control",
+              to: "/docs/2022/cac/",
+            },
+          ],
+        },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'GitHub',
-              href: 'https://github.com/Code4GovTech',
+              label: "GitHub",
+              href: "https://github.com/Code4GovTech",
             },
             {
-              label: 'Discord',
-              href: 'https://discord.gg/XeNVhYV4',
+              label: "Discord",
+              href: "https://discord.gg/XeNVhYV4",
             },
             {
-              label: 'Main home page',
-              href: 'http://tech.samagragovernance.in/',
+              label: "Main home page",
+              href: "http://tech.samagragovernance.in/",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'YouTube',
-              href: 'https://www.youtube.com/channel/UCfkXErS-f87xUQkmSKSC8bg',
-            },            
+              label: "YouTube",
+              href: "https://www.youtube.com/channel/UCfkXErS-f87xUQkmSKSC8bg",
+            },
             {
-              label: 'Getting started',
-              href: 'https://github.com/Code4GovTech/C4GT/wiki',
+              label: "Getting started",
+              href: "https://github.com/Code4GovTech/C4GT/wiki",
             },
           ],
         },
@@ -84,22 +88,18 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/Code4GovTech/C4GT/edit/main/',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/Code4GovTech/C4GT/edit/main/",
         },
         blog: {
           showReadingTime: true,
-          editUrl:
-            'https://github.com/Code4GovTech/C4GT/edit/main/',
+          editUrl: "https://github.com/Code4GovTech/C4GT/edit/main/",
         },
         theme: {
-          customCss: [
-            require.resolve('./src/css/custom.css')
-          ],
+          customCss: [require.resolve("./src/css/custom.css")],
         },
       },
     ],
